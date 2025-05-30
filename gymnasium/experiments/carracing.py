6a276471c05161615f87f415db6431c405c3b070
Julius Freiny <julius.freiny@gmail.com>
2025-05-31 00:30:39 +0200

    Added saving and loading checkpoints

--- gymnasium/experiments/carracing.py
+++ gymnasium/experiments/carracing.py
@@ -11,6 +11,21 @@ from torch.utils.tensorboard import SummaryWriter
 
 from agents.carracing_agent import MichaelSchumacherDiscrete, DQN
 from utils.dataclasses import Replay
+def save_checkpoint(
+        agent: DQN,
+        episode_idx: int,
+        step_counter: int,
+        checkpoint_name: str
+):
+    save_dict = {
+            'policy_network_state_dict': agent.policy_network.state_dict(),
+            'target_network_state_dict': agent.target_network.state_dict(),
+            'optimizer_state_dict': agent.optimizer.state_dict(),
+            'episode_idx': episode_idx,
+            'epsilon': agent.epsilon,
+            'step_counter': step_counter
+        }
+    torch.save(save_dict, f"checkpoints/{checkpoint_name}.pth")
 
 BATCH_SIZE = 64
 REPLAY_BUFFER_RESET_STEPS = 1000
@@ -26,6 +41,13 @@ writer = SummaryWriter(log_dir="runs/carracing_experiment")
 
 NUM_EPISODES = 100
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+load_episode = 0
+checkpoint_path = f"checkpoints/policy_full_ep{load_episode}.pth"
+checkpoint = None
+if os.path.exists(checkpoint_path):
+    checkpoint = torch.load(checkpoint_path, map_location=device)
+
+NUM_EPISODES = 1000
 NUM_TIMESTEPS = 1000
 MAX_REPLAY_BUFFER_LENGTH = 10_000
 
@@ -45,12 +67,32 @@ agent = MichaelSchumacherDiscrete(
     summary_writer=writer,
     device=device
 )
+
+start_step_counter = 0
+start_episode_number = 0
+
+if checkpoint is not None:
+    agent.policy_network.load_state_dict(checkpoint['policy_network_state_dict'])
+    agent.policy_network.eval()
+    agent.target_network.load_state_dict(checkpoint['target_network_state_dict'])
+    agent.target_network.eval()
+    agent.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    agent.epsilon = checkpoint['epsilon']
+    start_episode_number = checkpoint['episode_idx']
+    start_step_counter = checkpoint['step_counter']
+empty_state = np.zeros((96, 96, 3), dtype=np.uint8)
+states_queue = deque(maxlen=3)
 replay_buffer = deque(maxlen=MAX_REPLAY_BUFFER_LENGTH)
-step_counter = 0
-epsilon_update_rate = 10
+step_counter = start_step_counter
+epsilon_update_rate = 1
 network_train_rate = 4
+checkpoint_save_rate = 10
+reward_history = []
+moving_reward_window = 10
+best_average_reward = float('-inf')
 
 for episode_idx in range(NUM_EPISODES):
+for episode_idx in range(start_episode_number, NUM_EPISODES):
     
     state, info = env.reset()
     cummulative_episode_reward = 0
@@ -84,5 +126,18 @@ for episode_idx in range(NUM_EPISODES):
     mean_episode_reward = cummulative_episode_reward / episode_timesteps
     writer.add_scalar("Mean Reward / Episode", mean_episode_reward, episode_idx)
     writer.add_scalar("Epsilon", agent.epsilon, episode_idx)
+
+    reward_history.append(mean_episode_reward)
+
+    if len(reward_history) >= moving_reward_window:
+        moving_reward_average = sum(reward_history[-moving_reward_window:])/moving_reward_window
+        if moving_reward_average > best_average_reward:
+            best_average_reward = moving_reward_average
+            save_checkpoint(agent=agent, episode_idx=episode_idx, step_counter=step_counter, checkpoint_name='policy_best')
+        writer.add_scalar("Best Average Reward", best_average_reward, episode_idx)
+
+    if episode_idx > 0 and episode_idx % checkpoint_save_rate == 0:
+        save_checkpoint(agent=agent, episode_idx=episode_idx, step_counter=step_counter, checkpoint_name=f'policy_full_ep{episode_idx}')
+        
 
 env.close()
